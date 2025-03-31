901f28822181d8d0ae683d56ca38c348f4931286
Yonas <yonasseyoum24@gmail.com>
2025-04-01 01:13:47 +0300

    add product module

---
+++ src/products/products.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+import { Product, ProductSchema } from 'src/schema/product.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
+  controllers: [ProductsController],
+  providers: [ProductsService],
+  exports: [MongooseModule],
+})
+export class ProductsModule {}
